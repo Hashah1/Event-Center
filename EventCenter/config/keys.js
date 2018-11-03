dd1a500240e361c2279b401378afceea3b7ae95a
Mian Hashim Shah <mian_hashim_shah@hotmail.com>
2018-11-02 21:41:09 -0700

    Added server.js file along with the backend database schema and CRUD routes

--- EventCenter/config/keys.js
+++ EventCenter/config/keys.js
@@ -1,4 +1,4 @@
 module.exports =
 {
-    mongoURI: 'mongodb://hashim:030317Friday@ds131903.mlab.com:31903/sample_eventcenter_db'
+    mongoURI: 'mongodb://hash1996:030317Friday@ds121373.mlab.com:21373/eventcenterdb'
 };
--- EventCenter/server.js
+++ EventCenter/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose'); // To interact with mongodb
 const bodyParser = require('body-parser'); // take requests and get data from body
+const cors = require('cors');
 
 // to access the api and the requests
-const items = require ('./routes/api/items');
 const app = express();
 
-
+app.use(cors());
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
@@ -19,8 +19,12 @@ mongoose
     .then (() => console.log('DB connected...'))
     .catch(err => console.log(err));
 
+const api = require ('./routes/api');
+
 // any url with /api/items will get the file 'items'
-app.use('/api/items', items);
+app.use('/api', api);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log (`Server started on ${port}`));
+
+module.exports = app

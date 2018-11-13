799c00eb5835692fe59f19fbf0b39ea0fdd98754
Mian Hashim Shah <mian_hashim_shah@hotmail.com>
2018-11-12 23:23:44 -0800

    Moved the 'form' template code into form.jsx, thereby cleaning up code in eventManager.jsx

--- EventCenter/client/src/components/eventManager.jsx
+++ EventCenter/client/src/components/eventManager.jsx
@@ -1,10 +1,16 @@
+//TODO: 
+// 1. form doesnt automatically clear when we hit submit
+// 2. When we hit clear, the page reloads
+
 import React from 'react';
-import { Col, Button, Form, FormGroup, Label, Input, TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, CardText, Row, Modal,ModalHeader , ModalFooter, ModalBody} from 'reactstrap';
+import { Col, Button, TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, CardText, Row, Modal, ModalHeader , ModalFooter, ModalBody} from 'reactstrap';
 import classnames from 'classnames';
 import axios from 'axios';
 import "./announcementsPage.css"
 import "./announcementsPage"
-import { AvForm, AvGroup, AvInput, AvFeedback,  } from 'availity-reactstrap-validation';
+import "./form"
+import EventForm from './form';
+
 
 export default class EventManager extends React.Component {
     constructor(props) {
@@ -72,51 +78,13 @@ export default class EventManager extends React.Component {
       });
     }
 
-    onChange_eventName = (e) => {
-      this.setState({
-        eventName: e.target.value,
-      });
-    }
-
-    onChange_eventDescription = (e) => {
-      this.setState({
-        eventDescription: e.target.value,
-      });
-    } 
-    onChange_eventDate= (e) => {
-      this.setState({
-        eventDate: e.target.value,
-      });
-    } 
-    
-    onChange_eventTime = (e) => {
-      this.setState({
-        eventTime: e.target.value,
-      });
-    } 
-
-    onChange_eventLocation = (e) => {
-      this.setState({
-        eventLocation: e.target.value,
-      });
-    }
-
-    onChange_eventHost = (e) => {
-      this.setState({
-        eventHost: e.target.value,
-      });
-    }
-
-    onChange_eventContact = (e) => {
-      this.setState({
-        eventContact: e.target.value,
-      });
-    }
-
-    // when user clicks submit in post tab
+    ///////////////////////////////////////////////////
+    // This function submits the form as a draft event
+    // when the user clicks "submit"
+    ///////////////////////////////////////////////////
     onClickPost = (e) => {
       e.preventDefault(); // Prevent reloading page 
-
+      console.log(e);
       // Store the state of current data into form
       const submitData = {
         status: 'draft',
@@ -132,7 +100,7 @@ export default class EventManager extends React.Component {
       // Post to DB with the current state
       axios.post('http://localhost:5000/api/form', submitData)
       .then((res) => {
-        console.log(res.data); // Log data onto console
+        console.log("Data written to DB: " + res.data); // Log data onto console
 
         const form = res.data.data; // Get data from the response
         
@@ -160,7 +128,54 @@ export default class EventManager extends React.Component {
       });
     } // End onClickPost
 
-    // when user deletes an event
+    ////////////////////////////////////////////////////////////
+    // This function clears the event 
+    // when user clicks "clear" on the form
+    ////////////////////////////////////////////////////////////
+    onClickClear = () => {
+      this.setState ({   
+        status: 'draft',     
+        eventName: '',
+        eventDescription: '',
+        eventDate: '',
+        eventTime: '',
+        eventLocation: '',
+        eventHost: '',
+        eventContact: '',
+
+        publishedEvents: [],
+        draftedEvents: []
+      })
+    } // End onClickClear
+    
+    ////////////////////////////////////////////////////////////
+    // This function edits the event
+    // Occurs when the user clicks on 'edit'
+    ////////////////////////////////////////////////////////////
+    editEvent = (event) => {
+      console.log ("clicked edit button on event: " );
+      console.log(event.eventName);
+
+      // Repopulate form
+      const new_state = {...this.state,
+                         status: this.state.status,
+                         eventName: this.state.eventName,
+                         eventDescription: this.state.eventDescription,
+                         eventDate: this.state.eventDate,
+                         eventTime: this.state.eventTime,
+                         eventLocation: this.state.eventLocation,
+                         eventHost: this.state.eventHost,
+                         eventContact: this.state.eventContact,
+
+                         publishedEvents: this.state.publishedEvents,
+                         draftedEvents: this.state.draftedEvents
+                         };
+        this.setState(new_state);
+    } // End editEvent
+
+    ////////////////////////////////////////////////////
+    // This function deletes the event from the database
+    ////////////////////////////////////////////////////
     onClickDelete = (id) => {
       console.log("onClickDelete: clicked delete with id: "+ id);
 
@@ -182,9 +197,13 @@ export default class EventManager extends React.Component {
       .catch(err => {
         console.log(err);
       });
-    }
+    } // End onClickDelete
 
-    // when user clicks publish in Drafts tab to mark event as "published"
+    ////////////////////////////////////////////////////////////
+    // This function changes the status of the event
+    // from "draft" to "published"
+    // Occurs when the user clicks on "move to published" button
+    ////////////////////////////////////////////////////////////
     moveToPublished = (id) => {
       console.log("clicked publish with id: " + id);
       axios.post('http://localhost:5000/api/form/update/' + id, {status: "published"})
@@ -207,38 +226,11 @@ export default class EventManager extends React.Component {
       });
     } // End onClickPublish
 
-    // Switch the tab to the "post" tab
-    // Pre-fill the form with the event's present data
-    // update
-
-    editEvent = (event) => {
-      console.log ("clicked edit button on event: " );
-      console.dir(event);
-
-      // Repopulate form
-      const new_state = {...this.state,
-                          status: event.status,
-                          eventName: event.eventName,
-                          eventDescription: event.eventDescription,
-                          eventDate: event.eventDate,
-                           eventTime: event.eventTime,
-                           eventLocation: event.eventLocation,
-                           eventHost: event.eventHost,
-                           eventContact: event.eventContact,
-
-                           publishedEvents: this.state.publishedEvents,
-                           draftedEvents: this.state.draftedEvents
-                           };
-        this.setState(new_state);
-          
-
-
-
-        console.dir(new_state);
-      
-    } // End editEvent
-
-    // when user clicks 'move to drafts' in published tab to mark event as "draft"
+    ////////////////////////////////////////////////////////////
+    // This function changes the status of the event
+    // from "published" to "draft"
+    // Occurs when the user clicks on "move to draft" button
+    ////////////////////////////////////////////////////////////
     moveToDrafts = (id) => {
       console.log("clicked 'move to drafts with id: " + id);
       axios.post('http://localhost:5000/api/form/update/' + id, {status: "draft"})
@@ -257,23 +249,40 @@ export default class EventManager extends React.Component {
       });
     } // End moveToDrafts
 
-    // Clear form when user hits "clear" in form
-    onClickClear = () => {
-      this.setState ({   
-        status: 'draft',     
-        eventName: '',
-        eventDescription: '',
-        eventDate: '',
-        eventTime: '',
-        eventLocation: '',
-        eventHost: '',
-        eventContact: '',
+    ///////////////////////////////////////////////////
+    // Functions to set the state for the form fields
+    ///////////////////////////////////////////////////
+    onChange_eventName = (e)=>{
+      this.setState({eventName: e });
+    }
 
-        publishedEvents: [],
-        draftedEvents: []
-      })
-    } // End onClickClear
+    onChange_eventDescription = (e)=>{
+      this.setState({eventDescription: e });
+    }
 
+    onChange_eventDate = (e)=>{
+      this.setState({eventDate: e });
+    }
+    
+    onChange_eventTime = (e)=>{
+      this.setState({eventTime: e });
+    }
+    
+    onChange_eventLocation = (e)=>{
+      this.setState({eventLocation: e });
+    }
+
+    onChange_eventHost = (e)=>{
+      this.setState({eventHost: e });
+    }
+
+    onChange_eventContact = (e)=>{
+      this.setState({eventContact: e });
+    }
+
+    ///////////////////////////////////////////////////
+    // All the rendering of eventManager is here //////
+    ///////////////////////////////////////////////////
     render() {
       return (
         <div >
@@ -337,62 +346,15 @@ export default class EventManager extends React.Component {
               <Row>
                 <Col sm="10">
                   <h4 align = "center" style = {{paddingTop: 5}}>Post New announcements</h4> <br></br>
-                  <Form>
-                  <FormGroup row>
-                    <Label for="EventName" sm={2} >Event Name</Label>
-                    <Col sm={10}>
-                      <Input type="text" value={this.eventName} onChange={this.onChange_eventName}name="EventName" id="EventName" placeholder="enter event name" required/>
-                      {/* <AvFeedback>this is invalid</AvFeedback> */}
-                    </Col>
-                  </FormGroup>
-                   
-                  <FormGroup row>
-                    <Label for="EventDescription" sm={2}>Event Description</Label>
-                    <Col sm={10}>
-                      <Input type="textarea" value={this.state.eventDescription} onChange={this.onChange_eventDescription} name="text" id="EventDescription" placeholder="enter event description"/>
-                    </Col>
-                  </FormGroup>
-                  
-                  <FormGroup row>
-                    <Label for="EventDate" sm={2}>Event Date</Label>
-                    <Col sm={10}>
-                    <Input type="date" value={this.state.eventDate} onChange={this.onChange_eventDate} name="date" id="EventDate" />
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label for="EventTime" sm={2}>Time</Label>
-                    <Col sm={10}>
-                    <Input type="time" value={this.state.eventTime} onChange={this.onChange_eventTime} name="time" id="EventTime" />
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label for="EventLocation" sm={2}>Event Location</Label>
-                    <Col sm={10}>
-                    <Input type="text" value={this.state.eventLocation} onChange={this.onChange_eventLocation} name="text" id="EventLocation" placeholder="123 xyz street" />
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label for="EventHost" sm={2}>Host?</Label>
-                    <Col sm={10}>
-                      <Input type="text" value={this.state.eventHost} onChange={this.onChange_eventHost} name="EventHost" id="EventHost" placeholder="John Doe" />
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label for="EventContactInfo" sm={2}>Host Email</Label>
-                    <Col sm={10}>
-                      <Input type="email" value={this.state.eventContact} onChange={this.onChange_eventContact} name="EventContactInfo" id="EventContactInfo" placeholder="JohnDoe@sjsu.edu" />
-                      
-                      <Col sm = {100} style = {{paddingTop:15}}>
-                      <Button color = "primary" onClick={this.onClickPost}block> Post to Drafts</Button>
-                      <Button color = "danger" onClick={this.onClickClear}block> Clear </Button>
-                      </Col>
-                    </Col>
-                  </FormGroup>
-                  </Form>                
+                    <EventForm postEvent = {(e) => this.onClickPost(e)}  clearEvent = {() => this.onClickClear()}
+                               onEventNameChange = {this.state.onChange_eventName}
+                               onEventDescriptionChange = {this.onChange_eventDescription}
+                               onEventDateChange = {this.onChange_eventDate}
+                               onEventTimeChange = {this.onChange_eventTime}
+                               onEventLocationChange = {this.onChange_eventLocation}
+                               onEventHostChange = {this.onChange_eventHost}
+                               onEventContactChange = {this.onChange_eventContact}
+                              />
                   </Col>
               </Row>
             </TabPane>
@@ -416,11 +378,18 @@ export default class EventManager extends React.Component {
                                     <ModalHeader toggle={this.edit_modal_toggle}>Edit your event</ModalHeader>
                                     <ModalBody>
                                        <div>
-                                         </div>
+                                         <EventForm postEvent = {(e) => this.editEvent(e)}  clearEvent = {() => this.onClickClear()}
+                                                    event = {event.eventName}
+                                                    onEventNameChange = {this.onChange_eventName}
+                                                    onEventDescriptionChange = {this.onChange_eventDescription}
+                                                    onEventDateChange = {this.onChange_eventDate}
+                                                    onEventTimeChange = {this.onChange_eventTime}
+                                                    onEventLocationChange = {this.onChange_eventLocation}
+                                                    onEventHostChange = {this.onChange_eventHost}
+                                                    onEventContactChange = {this.onChange_eventContact}
+                                                    />/>
+                                      </div>
                                     </ModalBody>
-                                    <ModalFooter>
-                                      <Button color="primary" onClick={this.edit_modal_toggle}>Save Changes</Button>{' '}
-                                    </ModalFooter>
                                   </Modal>
                                   </div>
                             <Button color="danger" onClick = {()=>this.onClickDelete(event._id)}>Delete</Button>
--- EventCenter/client/src/components/form.jsx
+++ EventCenter/client/src/components/form.jsx
@@ -1,65 +1,130 @@
-// import React  from 'react';
-// import { Col, Button, Form, FormGroup, Label, Input, TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, CardText, Row, Modal,ModalHeader , ModalFooter, ModalBody} from 'reactstrap';
+///////////////////////////////////////////////////
+// This component is responsible for displaying the
+// event submission form and any state changes 
+// Associated with it
+///////////////////////////////////////////////////
+
+import React  from 'react';
+import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
-// const MyForm = (props) => { 
+export default class EventForm extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("this is from parent");
+        console.log(props.event);
+        //console.dir( this.props.onEventNameChange);
 
-//    return  (
-//             <Form>
-//                   <FormGroup row>
-//                     <Label for="EventName" sm={2}>Event Name</Label>
-//                     <Col sm={10}>
-//                       <Input type="text" value={this.eventName} onChange={this.onChange_eventName}name="EventName" id="EventName" placeholder="enter event name" />
-//                     </Col>
-//                   </FormGroup>
-                   
-//                   <FormGroup row>
-//                     <Label for="EventDescription" sm={2}>Event Description</Label>
-//                     <Col sm={10}>
-//                       <Input type="textarea" value={this.state.eventDescription} onChange={this.onChange_eventDescription} name="text" id="EventDescription" placeholder="enter event description"/>
-//                     </Col>
-//                   </FormGroup>
-                  
-//                   <FormGroup row>
-//                     <Label for="EventDate" sm={2}>Event Date</Label>
-//                     <Col sm={10}>
-//                     <Input type="date" value={this.state.eventDate} onChange={this.onChange_eventDate} name="date" id="EventDate" />
-//                     </Col>
-//                   </FormGroup>
+    // Set state with parent state//
+        this.state = {
+            activeTab: this.props.activeTab,
+            modal: this.props.modal,
+            
+            status: this.props.status,
+            eventName: this.props.eventName,
+            eventDescription: this.props.eventDescription,
+            eventDate: this.props.eventDate,
+            eventTime: this.props.eventTime,
+            eventLocation: this.props.eventLocation,
+            eventHost: this.props.eventHost,
+            eventContact: this.props.eventContact,
+    
+            publishedEvents: this.props.publishedEvents,
+            draftedEvents: this.props.draftedEvents
+          };
+    } // End Constructor
 
-//                   <FormGroup row>
-//                     <Label for="EventTime" sm={2}>Time</Label>
-//                     <Col sm={10}>
-//                     <Input type="time" value={this.state.eventTime} onChange={this.onChange_eventTime} name="time" id="EventTime" />
-//                     </Col>
-//                   </FormGroup>
+    ///////////////////////////////////////////////////
+    // Functions to set the state for the form fields//
+    ///////////////////////////////////////////////////
+    onChange_eventName = (e) => {
+        this.props.onEventNameChange(e.target.value);
+    }
 
-//                   <FormGroup row>
-//                     <Label for="EventLocation" sm={2}>Event Location</Label>
-//                     <Col sm={10}>
-//                     <Input type="text" value={this.state.eventLocation} onChange={this.onChange_eventLocation} name="text" id="EventLocation" placeholder="123 xyz street" />
-//                     </Col>
-//                   </FormGroup>
+    onChange_eventDescription = (e) => {
+    this.props.onEventDescriptionChange(e.target.value);
+    }
 
-//                   <FormGroup row>
-//                     <Label for="EventHost" sm={2}>Host?</Label>
-//                     <Col sm={10}>
-//                       <Input type="text" value={this.state.eventHost} onChange={this.onChange_eventHost} name="EventHost" id="EventHost" placeholder="John Doe" />
-//                     </Col>
-//                   </FormGroup>
+    onChange_eventDate = (e) => {
+    this.props.onEventDateChange(e.target.value);
+    }
+    
+    onChange_eventTime = (e) => {
+    this.props.onEventTimeChange(e.target.value);
+    } 
 
-//                   <FormGroup row>
-//                     <Label for="EventContactInfo" sm={2}>Host Email</Label>
-//                     <Col sm={10}>
-//                       <Input type="email" value={this.state.eventContact} onChange={this.onChange_eventContact} name="EventContactInfo" id="EventContactInfo" placeholder="JohnDoe@sjsu.edu" />
-                      
-//                       <Col sm = {100} style = {{paddingTop:15}}>
-//                       <Button color = "primary" onClick={this.onClickPost}block> Post to Drafts</Button>
-//                       <Button color = "danger" onClick={this.onClickClear}block> Clear </Button>
-//                       </Col>
-//                     </Col>
-//                   </FormGroup> }
-//                   </Form>
-//    )}
+    onChange_eventLocation = (e) => {
+    this.props.onEventLocationChange(e.target.value);
+    }
 
-// export default MyForm;
+    onChange_eventHost = (e) => {
+    this.props.onEventHostChange(e.target.value);
+    }
+
+    onChange_eventContact = (e) => {
+    this.props.onEventContactChange(e.target.value);
+    }
+
+    ///////////////////////////////////////////////////
+    /////////////// Display form here /////////////////
+    ///////////////////////////////////////////////////    
+    render () {
+        return  (
+        // Displays form to submit event
+        <Form>
+                <FormGroup row>
+                <Label for="EventName" sm={2}>Event Name</Label>
+                <Col sm={10}>
+                    <Input type="text" value={this.eventName} onChange={this.onChange_eventName}name="EventName" id="EventName" placeholder="enter event name" />
+                </Col>
+                </FormGroup>
+                
+                <FormGroup row>
+                <Label for="EventDescription" sm={2}>Event Description</Label>
+                <Col sm={10}>
+                    <Input type="textarea" value={this.eventDescription} onChange={this.onChange_eventDescription} name="text" id="EventDescription" placeholder="enter event description"/>
+                </Col>
+                </FormGroup>
+                
+                <FormGroup row>
+                <Label for="EventDate" sm={2}>Event Date</Label>
+                <Col sm={10}>
+                <Input type="date" value={this.eventDate} onChange={this.onChange_eventDate} name="date" id="EventDate" />
+                </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                <Label for="EventTime" sm={2}>Time</Label>
+                <Col sm={10}>
+                <Input type="time" value={this.eventTime} onChange={this.onChange_eventTime} name="time" id="EventTime" />
+                </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                <Label for="EventLocation" sm={2}>Event Location</Label>
+                <Col sm={10}>
+                <Input type="text" value={this.eventLocation} onChange={this.onChange_eventLocation} name="text" id="EventLocation" placeholder="123 xyz street" />
+                </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                <Label for="EventHost" sm={2}>Host?</Label>
+                <Col sm={10}>
+                    <Input type="text" value={this.eventHost} onChange={this.onChange_eventHost} name="EventHost" id="EventHost" placeholder="John Doe" />
+                </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                <Label for="EventContactInfo" sm={2}>Host Email</Label>
+                <Col sm={10}>
+                    <Input type="email" value={this.eventContact} onChange={this.onChange_eventContact} name="EventContactInfo" id="EventContactInfo" placeholder="JohnDoe@sjsu.edu" />
+
+                    <Col sm = {100} style = {{paddingTop:15}}>
+                        <Button color = "primary" onClick = {(e) => this.props.postEvent(e)} block> Post to Drafts </Button>
+                        <Button color = "danger" onClick = {this.props.onClickClear} block> Clear </Button>
+                    </Col>
+                </Col>
+                </FormGroup>
+        </Form>
+        )}
+}
